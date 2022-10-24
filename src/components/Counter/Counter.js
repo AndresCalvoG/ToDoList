9cba2bd9b45ff76bdf008b7c47d2472fbd9b0e0b
AndresCalvoG <andrescalvo9407@gmail.com>
2022-10-23 20:15:18 -0500

    main styles

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Styled from "styled-components";
 
+const Header = Styled.h1`
+  font-size: 2.4rem;
+  text-align: center;
+`;
 function Counter() {
-  return <h1>Has completado 2 de 3 items</h1>;
+  return <Header>Has completado 2 de 3 items</Header>;
 }
 
 export { Counter };
