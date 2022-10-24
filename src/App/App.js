7c266f90bb1dc4eec5b3e6b303a403ada0f24f06
AndresCalvoG <andrescalvo9407@gmail.com>
2022-10-24 12:19:49 -0500

    end step 16 react context

--- src/App/App.js
+++ src/App/App.js
@@ -1,60 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { AppUI } from "./AppUI";
-
-import { useLocalStorage } from "../customHooks/useLocalStorage";
+import { Provider } from "../context/index";
 
 function App() {
-  const {
-    item: todos,
-    saveItem: saveTodos,
-    loading,
-    error,
-  } = useLocalStorage("TODOS_V1", []);
-  const [searchValue, setSearchValue] = useState("");
-
-  const completedTodos = todos.filter((todo) => !!todo.completed).length;
-  const totalTodos = todos.length;
-
-  let searchedTodos = [];
-
-  if (!searchValue.length >= 1) {
-    searchedTodos = todos;
-  } else {
-    searchedTodos = todos.filter((todo) => {
-      const todotext = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todotext.includes(searchText);
-    });
-  }
-
-  const completeTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
-    const newTodos = [...todos];
-    newTodos[todoIndex].completed = true;
-    saveTodos(newTodos);
-  };
-
-  const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
-    const newTodos = [...todos];
-    newTodos.splice(todoIndex, 1);
-    saveTodos(newTodos);
-  };
-
-  useEffect(() => {}, []);
-
   return (
-    <AppUI
-      loading={loading}
-      error={error}
-      totalTodos={totalTodos}
-      completedTodos={completedTodos}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      searchedTodos={searchedTodos}
-      completeTodo={completeTodo}
-      deleteTodo={deleteTodo}
-    />
+    <Provider>
+      <AppUI />
+    </Provider>
   );
 }
 
--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -4,36 +4,31 @@ import { Searcher } from "../components/Searcher/Searcher";
 import { List } from "../components/List/List";
 import { Item } from "../components/Item/Item";
 import { NewItemButton } from "../components/NewItemButton/NewItemButton";
+import { Context } from "../context/index";
 
-function AppUI({
-  loading,
-  error,
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}) {
+function AppUI() {
   return (
     <>
-      <Counter total={totalTodos} completed={completedTodos} />
-      <Searcher searchValue={searchValue} setSearchValue={setSearchValue} />
-      <List>
-        {error && <p>Desesperate..</p>}
-        {loading && <p>Cargando...</p>}
-        {!loading && !searchedTodos.length && <p>Crea un todo</p>}
-        {searchedTodos.map((todo) => (
-          <Item
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </List>
+      <Counter />
+      <Searcher />
+      <Context.Consumer>
+        {({ error, loading, searchedTodos, completeTodo, deleteTodo }) => (
+          <List>
+            {error && <p>Desesperate..</p>}
+            {loading && <p>Cargando...</p>}
+            {!loading && !searchedTodos.length && <p>Crea un todo</p>}
+            {searchedTodos.map((todo) => (
+              <Item
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </List>
+        )}
+      </Context.Consumer>
       <NewItemButton />
     </>
   );
