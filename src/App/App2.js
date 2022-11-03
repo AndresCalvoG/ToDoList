68e79d31fb6ea56c39af07be7418bb2150e28131
AndresCalvoG <andrescalvo9407@gmail.com>
2022-11-02 22:31:04 -0500

    end step 3 classState and useState

--- src/App/App2.js
+++ src/App/App2.js
@@ -1,30 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Styled from "styled-components";
 
 const App = Styled.div`
   width: 100vw;
-  
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
 `;
 
-function UseState() {
+function UseState({ name }) {
+  const [error, setError] = useState(false);
+
   return (
     <div>
-      <h1>Eliminar UseState</h1>
+      <h1>Eliminar {name}</h1>
       <p>Por favor, escribe el codigo de seguridad</p>
+      {error && <p>Error: el codigo es incorrecto</p>}
       <input placeholder="Codigo de seguridad" />
-      <button>Comprobar</button>
+      <button onClick={() => setError(!error)}>Comprobar</button>
     </div>
   );
 }
 
 class ClassState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1>Eliminar ClassState</h1>
+        <h1>Eliminar {this.props.name}</h1>
         <p>Por favor, escribe el codigo de seguridad</p>
+        {this.state.error && <p>Error: el codigo es incorrecto</p>}
         <input placeholder="Codigo de seguridad" />
-        <button>Comprobar</button>
+        <button onClick={() => this.setState({ error: !this.state.error })}>
+          Comprobar
+        </button>
       </div>
     );
   }
@@ -33,8 +50,8 @@ class ClassState extends React.Component {
 function App2() {
   return (
     <App>
-      <UseState />
-      <ClassState />
+      <UseState name="Use State" />
+      <ClassState name="Class State" />
     </App>
   );
 }
