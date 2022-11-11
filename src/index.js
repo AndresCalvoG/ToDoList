61932f35f5d3bba49fea22217482209356de87d1
AndresCalvoG <andrescalvo9407@gmail.com>
2022-11-10 21:35:11 -0500

    end step 16 migracion

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-//import App from "./App/App";
-import App2 from "./App/App2";
+import App from "./App/App";
+//import App2 from "./App/App2";
 import reportWebVitals from "./reportWebVitals";
-//import App from "./notas";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App2 />
+    <App />
   </React.StrictMode>
 );
 
